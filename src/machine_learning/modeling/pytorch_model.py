72e0789b12022e994bb7608965ad3109cdd8ca3e
ChrisPrein <79170851+ChrisPrein@users.noreply.github.com>
2022-05-11 00:12:25 +0200

    adds device to pytorch model

--- src/machine_learning/modeling/pytorch_model.py
+++ src/machine_learning/modeling/pytorch_model.py
@@ -6,8 +6,11 @@ import torch
 import torch.nn as nn
 
 class PytorchModel(Model[TInput, TTarget]):
-    def __init__(self, pytorch_module: nn.Module, loss_function: nn.Module, optimizer: Union[torch.optim.Optimizer, Callable[[], torch.optim.Optimizer]], scheduler: Optional[Union[torch.optim.lr_scheduler._LRScheduler, Callable[[], torch.optim.lr_scheduler._LRScheduler]]] = None):
+    def __init__(self, pytorch_module: nn.Module, device: torch.device, loss_function: nn.Module, optimizer: Union[torch.optim.Optimizer, Callable[[], torch.optim.Optimizer]], scheduler: Optional[Union[torch.optim.lr_scheduler._LRScheduler, Callable[[], torch.optim.lr_scheduler._LRScheduler]]] = None):
         self.inner_module: nn.Module = pytorch_module
+
+        self.device: torch.device = device
+
         self.loss_function: nn.Module = loss_function
 
         self.optimizer_factory: Optional[Callable[[], torch.optim.Optimizer]] = optimizer if not isinstance(optimizer, torch.optim.Optimizer) else None
