9ff30e5bf970b28d58d1f0c3fd5a5a56e9c35541
ChrisPrein <79170851+ChrisPrein@users.noreply.github.com>
2022-12-29 17:19:14 +0100

    minor changes

--- src/machine_learning/evaluation/__init__.py
+++ src/machine_learning/evaluation/__init__.py
@@ -6,3 +6,15 @@ from evaluation_service import *
 from evaluator import *
 from multi_evaluation_context import *
 from multi_metric import *
+
+import default_evaluation_plugin
+import default_evaluation_service
+import evaluation_context
+import evaluation_metric
+import evaluation_service
+import evaluator
+import multi_evaluation_context
+import multi_metric
+
+__all__ = default_evaluation_plugin.__all__ + default_evaluation_service.__all__ + evaluation_context.__all__ + evaluation_metric.__all__ + \
+    evaluation_service.__all__ + evaluator.__all__ + multi_evaluation_context.__all__ + multi_metric.__all__
